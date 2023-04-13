f26027e91b57fdadd0a44a3aed30dad604226140
Oluwafemi Kayode <mrlapinho@gmail.com>
2023-04-14 00:05:57 +0100

    Setup project with vite, tailwind and alpine js.

---
+++ main.js
@@ -0,0 +1,12 @@
+import Alpine from "alpinejs";
+
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+
+import "./style.css";
+
+import { products } from "./data.js";
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
