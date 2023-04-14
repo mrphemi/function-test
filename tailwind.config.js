faa277eb5ba8e1e1e68e5980bd915779910ff4ba
Oluwafemi Kayode <mrlapinho@gmail.com>
2023-04-14 01:28:00 +0100

    Add top navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ export default {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
+      colors: {
+        "function-green": "#19291C",
+        "function-blue": "#282B3A",
+        "function-light-grey": {
+          default: "#F1F1F2",
+          alt: "#808080",
+        },
+        "function-dark-grey": {
+          default: "#6B6D7A",
+          alt: "#91939F",
+        },
+      },
     },
   },
   plugins: [],
