fa24188ec825bb49e47795466d00051f0165f370
Oluwafemi Kayode <mrlapinho@gmail.com>
2023-04-15 10:26:18 +0100

    Add products section

--- data.js
+++ data.js
@@ -1,1 +1,66 @@
-export const products = [];
+export const products = () => ({
+  list: [
+    {
+      id: 1,
+      name: "Fredo Jacket",
+      price: "£230",
+      image_url: "/assets/product-image-1.png",
+    },
+
+    {
+      id: 2,
+      name: "Powell Jacket",
+      price: "£350",
+      image_url: "/assets/product-image-2.png",
+    },
+    {
+      id: 3,
+      name: "Lazar Jacket in Denim",
+      price: "£395",
+      image_url: "/assets/product-image-3.png",
+    },
+    {
+      id: 4,
+      name: "Lazar Jacket in Black",
+      price: "£395",
+      image_url: "/assets/product-image-4.png",
+    },
+    {
+      id: 5,
+      name: "Lazar Leather Jacket",
+      price: "£395",
+      image_url: "/assets/product-image-5.png",
+    },
+    {
+      id: 6,
+      name: "Fredo Jacket",
+      price: "£230",
+      image_url: "/assets/product-image-1.png",
+    },
+
+    {
+      id: 7,
+      name: "Powell Jacket",
+      price: "£350",
+      image_url: "/assets/product-image-2.png",
+    },
+    {
+      id: 8,
+      name: "Lazar Jacket in Denim",
+      price: "£395",
+      image_url: "/assets/product-image-3.png",
+    },
+    {
+      id: 9,
+      name: "Lazar Jacket in Black",
+      price: "£395",
+      image_url: "/assets/product-image-4.png",
+    },
+    {
+      id: 10,
+      name: "Lazar Leather Jacket",
+      price: "£395",
+      image_url: "/assets/product-image-5.png",
+    },
+  ],
+});
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import Alpine from "alpinejs";
 
+import { products } from "./data.js";
+
 import "@fontsource/inter/300.css";
 import "@fontsource/inter/400.css";
 
 import "./style.css";
 
-import { products } from "./data.js";
-
-window.Alpine = Alpine;
+Alpine.data("products", products);
 
 Alpine.start();
